ec5f6275f3a6db345ba556f0acc2fc7240c7d0f4
Mymint080417 <kanitapromjeen@gmail.com>
2025-02-14 22:45:52 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-import './style.css';
 
-document.body.style.backgroundImage = "url('background.jpg')";
-document.body.style.backgroundSize = "cover";
-document.body.style.backgroundPositio
-// ลงสีพื้นหลังแบบสุ่ม
-function randomColor() {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
-    return `rgb(${r}, ${g}, ${b})`;
-  }
